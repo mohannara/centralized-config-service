c53699ef976994505e1e12c0fadeb5aaf7570c45
mohannara <mohan.narangoda@gmail.com>
2019-10-07 20:32:04 +0530

    update repo

--- src/main/java/lk/dialog/crm/config/service/CentralizedConfigServiceApplication.java
+++ src/main/java/lk/dialog/crm/config/service/CentralizedConfigServiceApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * @author Mohan_02392
+ *
+ */
+
 @EnableConfigServer
 @SpringBootApplication
 public class CentralizedConfigServiceApplication {
